30022ee422b07e350dca1298750206fc75168d1e
ADMIN <ADMIN@LAB1-MAY29>
2024-06-10 11:17:07 +0700

    Add project files.

---
+++ Csharp_hinhtron/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Csharp_hinhtron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lblkq = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(186, 265);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(120, 48);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Diện Tích";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(236, 190);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(198, 36);
+            this.txt1.TabIndex = 4;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(350, 265);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(120, 48);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Chu Vi";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lblkq
+            // 
+            this.lblkq.AutoSize = true;
+            this.lblkq.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblkq.Location = new System.Drawing.Point(67, 462);
+            this.lblkq.Name = "lblkq";
+            this.lblkq.Size = new System.Drawing.Size(99, 29);
+            this.lblkq.TabIndex = 5;
+            this.lblkq.Text = "Kết Quả";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bán Kính";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Csharp_hinhtron.Properties.Resources.tải_xuống;
+            this.pictureBox1.Location = new System.Drawing.Point(220, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 160);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(551, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 169);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(24, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(113, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Diện tích";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(24, 110);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Chu vi";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 65);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1591, 643);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblkq);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "`";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblkq;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Csharp_hinhtron/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_hinhtron
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double bankinh = Convert.ToDouble(txt1.Text);
+            double dientich;
+            dientich = bankinh * bankinh * Math.PI;
+            lblkq.Text = "-Diện tích" + Convert.ToDouble(dientich);
+
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            double bankinh = Convert.ToDouble(txt1.Text);
+            string thongbao = "";
+            double chuvi;
+            chuvi = bankinh * 2 * Math.PI;
+            lblkq.Text = "-Chu vi" + Convert.ToDouble(chuvi);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        { double bankinh=Convert.ToDouble(txt1.Text);
+            if(radioButton1.Checked)
+            { double chuvi=bankinh*2*Math.PI;
+            String thongbao= "-Diện tích" + Convert.ToString(chuvi);
+                MessageBox.Show(thongbao, "Thông Báo",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            }
+            if (radioButton2.Checked)
+            {
+                double dientich=bankinh * bankinh * Math.PI;
+                string thongbao = "-Diện tích"+Convert.ToString(dientich);
+
+            }
+            
+        }
+    
+
